8f3f303e452e61ffe9809d132f74b43fde372ffb
Anthony Charpenay <anthony.charpenay@proton.me>
2025-01-16 15:35:40 +0100

    Move guard close outside of thread

--- server/main.lua
+++ server/main.lua
@@ -1,20 +1,20 @@
 AddEventHandler('onResourceStart', function()
   for key, values in pairs(Scripts) do
-    CreateThread(function()
-      while true do
-        local currentDay = os.date("%d/%m/%Y")
-        
-        if not currentDay == values.date then return end
+    if currentDay == values.date then
+      CreateThread(function()
+        while true do
+          local currentDay = os.date("%d/%m/%Y")
 
-        local currentTime = os.date("%H")
+          local currentTime = os.date("%H")
 
-        if currentTime == values.time then
-          values.script()
-          print(string.format("%s executed : %s", Config.debugPrefix, key))
-          return
+          if currentTime == values.time then
+            values.script()
+            print(string.format("%s executed : %s", Config.debugPrefix, key))
+            return
+          end
+
+          wait(Config.delay)
         end
-        
-        wait(Config.delay)
       end
     end)
   end
